e93a3f76185100f52908154c513b68f7755da6e9
claudemirramosferreira@gmail.com <claudemirramosferreira@gmail.com>
2021-01-19 16:51:58 -0400

    iniciei o o modulo de vendas

--- src/app/admin/venda/venda-routing.module.ts
+++ src/app/admin/venda/venda-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateVendaComponent } from './create-venda/create-venda.component';
 
-
-
 const routes: Routes = [
+
   {
     path: '',
     component: CreateVendaComponent
-}
+  },
+  {
+    path: 'create',
+    component: CreateVendaComponent
+  }
 ];
 
 @NgModule({
--- src/app/admin/venda/venda.module.ts
+++ src/app/admin/venda/venda.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { VendaRoutingModule } from './venda-routing.module';
 import { CreateVendaComponent } from './create-venda/create-venda.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from 'src/app/shared/material/material.module';
 
 
 @NgModule({
   declarations: [CreateVendaComponent],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    MaterialModule,
     VendaRoutingModule
   ]
 })
